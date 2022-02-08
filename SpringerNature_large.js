8ae632c3fa2b681c55c8c6080b30494caa5d5294
Hannah Kleidermacher <hck@berkeley.edu>
2022-02-08 11:15:57 -0800

    Springer Nature abstract collection: large & small versions to handle data collection from large/small quantities per year in order to stay within daily/minute hit limits

---
+++ SpringerNature_large.js
@@ -0,0 +1,73 @@
+var ENDLOOP = 10000/100 // no. total articles divided by no. articles/page
+var YEAR = 2006;
+var PAGELENGTH = 100;
+var START = 1;
+
+var FNAMES = {};
+var numAbstracts = {};
+var ids = {};
+
+var fs = require("fs");
+var https = require("https");
+
+
+function collect_abstracts(json) {
+    var articles = json.records;
+    console.log(json.result)
+    //console.log(json.records)
+    if (articles !== undefined) {
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            if (article.hasOwnProperty("abstract")) {
+                var abstract = article.abstract;
+                var fn = "SpringerNature/" + YEAR;
+                fs.appendFileSync(fn, abstract, "utf8");
+            }
+        }
+    }
+}
+
+var j = 0
+function execute () {
+    if (j > ENDLOOP) return;
+
+    setTimeout(function () {
+        for (var i = j; i < j+150; i++) {
+            var url = "https://api.springernature.com/meta/v2/json?q="
+                    +"(pub:%22Nature%20Communications%22%20OR%20"
+                    +"pub:Nature%20OR%20"
+                    +"pub:%22Nature%20Materials%22%20OR%20"
+                    +"pub:%22Nature%20Nanotechnology%22%20OR%20"
+                    +"pub:%22Nature%20Photonics%22%20OR%20"
+                    +"pub:%22Nature%20Physicsy%22%20OR%20"
+                    +"pub:%22Nature%20Reviews%20Materials%22%20OR%20"
+                    +"pub:%22Nature%20Reviews%20Physics%22%20OR%20"
+                    +"pub:%22Communications%20Engineering%22%20OR%20"
+                    +"pub:%22Communications%20Physics%22%20OR%20"
+                    +"pub:%22npj%20Computational%20Materials%22%20OR%20"
+                    +"pub:%22npj%20Quantum%20Materials%22%20OR%20"
+                    +"pub:%22Scientific%20Reports%22%20OR%20"
+                    +"pub:%22Nature%20Energy%22)"
+                    +" year:"+YEAR
+                    +"&p="+PAGELENGTH+"&s="+(START+PAGELENGTH*i)
+                    +"&api_key=86e121ae7080eb66373361f62f04b07b";
+            https.get(url, function (res) {
+                res.setEncoding("utf8")
+                var body = "";
+                res.on("data", function (data) {
+                    if (typeof body !== 'undefined') {
+                        body += data;
+                    }
+                });
+                res.on("end", function () {
+                    body = JSON.parse(body);
+                    collect_abstracts(body);
+                });
+            });
+        }
+        j += 150
+        execute()
+    }, 36000);
+}
+
+execute()
---
+++ SpringerNature_small.js
@@ -0,0 +1,66 @@
+//var ENDLOOP = 10000/100 // no. total articles divided by no. articles/page
+//var YEAR = 1994;
+var STARTYEAR = 1944;
+var ENDYEAR = 1993;
+var PAGELENGTH = 100;
+var START = 1;
+
+var FNAMES = {};
+var numAbstracts = {};
+var ids = {};
+
+var fs = require("fs");
+var https = require("https");
+
+
+function collect_abstracts(json, yr) {
+    var articles = json.records;
+    console.log(json.result)
+    //console.log(json.records)
+    if (articles !== undefined) {
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            if (article.hasOwnProperty("abstract")) {
+                var abstract = article.abstract;
+                var fn = "SpringerNature/" + yr;
+                fs.appendFileSync(fn, abstract, "utf8");
+            }
+        }
+    }
+}
+
+for (var y = STARTYEAR; y <= ENDYEAR; y++) {
+    for (var i = 0; i < 30; i++) {
+        var url = "https://api.springernature.com/meta/v2/json?q="
+                +"(pub:%22Nature%20Communications%22%20OR%20"
+                +"pub:Nature%20OR%20"
+                +"pub:%22Nature%20Materials%22%20OR%20"
+                +"pub:%22Nature%20Nanotechnology%22%20OR%20"
+                +"pub:%22Nature%20Photonics%22%20OR%20"
+                +"pub:%22Nature%20Physicsy%22%20OR%20"
+                +"pub:%22Nature%20Reviews%20Materials%22%20OR%20"
+                +"pub:%22Nature%20Reviews%20Physics%22%20OR%20"
+                +"pub:%22Communications%20Engineering%22%20OR%20"
+                +"pub:%22Communications%20Physics%22%20OR%20"
+                +"pub:%22npj%20Computational%20Materials%22%20OR%20"
+                +"pub:%22npj%20Quantum%20Materials%22%20OR%20"
+                +"pub:%22Scientific%20Reports%22%20OR%20"
+                +"pub:%22Nature%20Energy%22)"
+                +" year:"+YEAR
+                +"&p="+PAGELENGTH+"&s="+(START+PAGELENGTH*i)
+                +"&api_key=86e121ae7080eb66373361f62f04b07b";
+        https.get(url, function (res) {
+            res.setEncoding("utf8")
+            var body = "";
+            res.on("data", function (data) {
+                if (typeof body !== 'undefined') {
+                    body += data;
+                }
+            });
+            res.on("end", function () {
+                body = JSON.parse(body);
+                collect_abstracts(body, y);
+            });
+        });
+    }
+}
