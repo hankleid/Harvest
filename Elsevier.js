7ab5de0454affcd43ef8dce58d57647e2a8d824b
Hannah Kleidermacher <hck@berkeley.edu>
2022-04-05 11:59:00 -0700

    attempt to add delay by using mapped promises. ready to run. not successful at adding the delays.

--- Elsevier.js
+++ Elsevier.js
@@ -1,71 +1,54 @@
-var STARTYEAR = 1950;
-var ENDYEAR = 2000;
-var INTERVAL = 1;
-var NUMPAGES = 2000;
+const axios = require('axios');
+const YEAR = 2022;
 
-var FNAMES = {};
-var numAbstracts = {};
-var ids = {};
-
-var fs = require("fs");
-var https = require("https");
-
-for (var y = STARTYEAR; y <= ENDYEAR; y += INTERVAL) {
-    for (var i = 0; i < INTERVAL; i++) {
-        FNAMES[y+i] = y + "_" + (y+INTERVAL-1) + ".txt";
-    }
-}
-
-function getFileName(year) {
-    return FNAMES[year];
-}
-
-function collect_abstracts(json) {
-    var articles = json.data;
-    for (var i = 0; i < articles.length; i++) {
-        var article = articles[i];
-        var year = article.date.substring(0, 4);
-        if (article.hasOwnProperty("abstract") && getFileName(year)) {
-            var abstract = article.abstract.value;
-            var id = article.journal.id;
-            if (ids[id] == undefined) {
-                ids[id] = 1;
-            } else {
-                ids[id] += 1;
-            }
-            if (numAbstracts[year] == undefined) {
-                numAbstracts[year] = 1;
-            } else {
-                numAbstracts[year] += 1;
-            }
-            var fn = "APS/" + getFileName(year);
-            fs.appendFileSync(fn, abstract, "utf8");
-        }
-    }
-    console.log(numAbstracts);
-    console.log(ids);
-}
-
-for (var i = 0; i <= 0; i++) {
-    var url = "https://api.elsevier.com/content/search/scopus?"
-            +"apiKey=e622388ba261e700b860d2572455d9d4"
-            +"&field=description"
+const idURL = "https://api.elsevier.com/content/search/scopus?"
+            +"query=LANGUAGE%28english%29"
+            +"&apiKey=e622388ba261e700b860d2572455d9d4"
+            +"&insttoken=288208f8f5ebac7eb3cd123901463335"
+            +"&view=STANDARD"
             +"&sort=pubyear"
-            +"date=2020-2021"
-            +"&count=1"
-            +"&start="+i
-    https.get(url, function (res) {
-        res.setEncoding("utf8")
-        var body = "";
-        res.on("data", function (data) {
-            if (typeof body !== 'undefined') {
-                body += data;
-            }
-        });
-        res.on("end", function () {
-            body = JSON.parse(body);
-            console.log(body)
-            //collect_abstracts(body);
-        });
-    });
+            +"&date="+YEAR
+            +"&count=10"
+            +"&start="+0;
+
+
+const delay = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
+const getInSeries = async (promises) => {
+  let results = [];
+  let i = 1;
+  for (let promise of promises) {
+    results.push(await delay().then(() => promise));
+    console.log("pushed " + i + "!")
+    i++;
+  }
+  return results;
+};
+
+async function getArticleIds(url) {
+    axios
+        .get(url)
+        .then(res => {
+            console.log("PAGE 1")
+            const articles = res.data['search-results'].entry;
+            const eids = articles.map((article) => article.eid);
+            console.log(eids);
+            getAbstracts(eids);
+        })
+        .catch((error) => console.error(error.response.status))
 }
+
+async function getAbstracts(eids) {
+    const promises = eids.map((eid) =>
+        axios
+            .get(`https://api.elsevier.com/content/abstract/eid/${eid}?apiKey=e622388ba261e700b860d2572455d9d4&insttoken=288208f8f5ebac7eb3cd123901463335`)
+            .then((res) => res.data['abstracts-retrieval-response'].coredata['dc:description'])
+            .catch((error) => console.error(error.response.status))
+    );
+    const results = await getInSeries(promises);
+    const abstracts = results.filter(el => {
+        return el !== undefined;
+    });
+    console.log(abstracts)
+}
+
+getArticleIds(idURL);
