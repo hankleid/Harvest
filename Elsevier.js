0ec37fd5d0f704baad50b25118531fff3fb4092a
Hannah Kleidermacher <hck@berkeley.edu>
2022-04-20 12:39:50 -0700

    collects 400 article abstracts & successfully puts in text file. took ~17mins: will take way too long to collect from all years. 400 can be changed to the actual # of articles returned by the API call - just need to edit code for that.

--- Elsevier.js
+++ Elsevier.js
@@ -1,54 +1,72 @@
 const axios = require('axios');
+const fs = require("fs");
 const YEAR = 2022;
+const PERPAGE = 200;
 
 const idURL = "https://api.elsevier.com/content/search/scopus?"
-            +"query=LANGUAGE%28english%29"
+            +"query=SUBJAREA%28ENGI+OR+MATE+OR+PHYS+OR+ENER%29"
             +"&apiKey=e622388ba261e700b860d2572455d9d4"
             +"&insttoken=288208f8f5ebac7eb3cd123901463335"
             +"&view=STANDARD"
             +"&sort=pubyear"
             +"&date="+YEAR
-            +"&count=10"
-            +"&start="+0;
+            +"&count="+PERPAGE
+            +"&start=";
 
 
-const delay = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
-const getInSeries = async (promises) => {
-  let results = [];
-  let i = 1;
-  for (let promise of promises) {
-    results.push(await delay().then(() => promise));
-    console.log("pushed " + i + "!")
-    i++;
-  }
-  return results;
+const delay = (ms = 0) => new Promise((r) => setTimeout(r, ms));
+
+const getAbstracts = async function (eids) {
+    let results = [];
+    for (let i = 0; i < eids.length; i++) {
+        await delay();
+        const res = await axios
+            .get(
+                `https://api.elsevier.com/content/abstract/eid/${eids[i]}?apiKey=e622388ba261e700b860d2572455d9d4&insttoken=288208f8f5ebac7eb3cd123901463335`
+            ).then(
+                console.log("called " + (i+1) + "!")
+            ).catch(
+                (error) => console.error(error.response.status));
+        results.push(res.data['abstracts-retrieval-response'].coredata['dc:description'])
+    }
+    abstracts = results.filter((element) => { return element !== undefined; });
+    return abstracts;
 };
 
-async function getArticleIds(url) {
-    axios
-        .get(url)
-        .then(res => {
-            console.log("PAGE 1")
-            const articles = res.data['search-results'].entry;
-            const eids = articles.map((article) => article.eid);
-            console.log(eids);
-            getAbstracts(eids);
-        })
-        .catch((error) => console.error(error.response.status))
+const processAbstracts = async function (texts) {
+    for (let text of texts) {
+        let fn = "Elsevier/" + YEAR;
+        fs.appendFileSync(fn, text, "utf8");
+    }
+    console.log("Abstracts from " + YEAR + " printed successfully.");
+    return;
 }
 
-async function getAbstracts(eids) {
-    const promises = eids.map((eid) =>
-        axios
-            .get(`https://api.elsevier.com/content/abstract/eid/${eid}?apiKey=e622388ba261e700b860d2572455d9d4&insttoken=288208f8f5ebac7eb3cd123901463335`)
-            .then((res) => res.data['abstracts-retrieval-response'].coredata['dc:description'])
-            .catch((error) => console.error(error.response.status))
-    );
-    const results = await getInSeries(promises);
-    const abstracts = results.filter(el => {
-        return el !== undefined;
-    });
-    console.log(abstracts)
+
+async function main() {
+    const res = await axios
+        .get(idURL+0)
+        .catch((error) => console.error(error));
+    console.log(res.data['search-results']['opensearch:totalResults']);
+    const totalNumArticles = 400;
+
+    let page = 0;
+    while (page*PERPAGE < totalNumArticles) { // STOP @ FINAL ARTICLE.
+        let thisURL = idURL + page*PERPAGE;
+        console.log(thisURL);
+        const res = await axios
+            .get(thisURL)
+            .catch((error) => console.error(error));
+
+        console.log("PAGE "+page);
+        const articles = res.data['search-results'].entry;
+        const eids = articles.map((article) => article.eid);
+        console.log(eids);
+        const abstracts = await getAbstracts(eids);
+        await processAbstracts(abstracts);
+
+        page++;
+    }
 }
 
-getArticleIds(idURL);
+main(idURL);
